bbccb4177814001c5953f4cb5870c5cc15d24b02
j129008 <j129008@gmail.com>
2016-02-18 15:34:57 +0800

    remove unicode

--- crawer.py
+++ crawer.py
@@ -4,19 +4,20 @@ from bs4 import BeautifulSoup
 data = []
 url = 'http://newtalk.tw/news/view/2008-08-28/'
 
-post = []
-fp = urllib.request.urlopen(url+str(2))
-soup = BeautifulSoup(fp , 'html.parser')
+for i in range(1,10):
+    post = []
+    fp = urllib.request.urlopen(url+str(i))
+    soup = BeautifulSoup(fp , 'html.parser')
 
-title = soup.select('.content_title')[0].string
-post.append(title)
+    title = soup.select('.content_title')[0].string
+    post.append(title)
 
-content = ''
-lineList = soup.txt.find_all('p')
-for line in lineList:
-    lineStr = line.string
-    if (lineStr != None) and ("連結" not in lineStr):
-        content += lineStr
-post.append(content)
-data.append(post)
+    content = ''
+    lineList = soup.txt.find_all('p')
+    for line in lineList:
+        lineStr = line.string
+        if (lineStr != None) and ("連結" not in lineStr):
+            content += lineStr.replace('\u3000','')
+    post.append(content)
+    data.append(post)
 print(data)
