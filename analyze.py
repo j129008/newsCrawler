fce6277bc60cf723e09e1bd7cceec2e392840f62
j129008 <j129008@gmail.com>
2016-02-21 18:40:59 +0800

    fb likes average

--- analyze.py
+++ analyze.py
@@ -1,24 +1,39 @@
 import pickle
-import jieba
-import jieba.analyse
-import operator
+# import jieba
+# import jieba.analyse
+# import operator
 
 data = []
 with open("./newsDataCn.pkl", 'rb') as fr:
     data = pickle.load(fr)
 
-allTag = []
-for i in range(1, 100):
-    allTag += jieba.analyse.extract_tags(data[i][1],topK=3)
+likes = dict()
+posts = dict()
+for i in range(len(data)):
+    authors = data[i][1].split("、")
+    likesCnt = data[i][2]
+    for author in authors:
+        try:
+            likes[author] += int(likesCnt)
+            posts[author] += 1
+        except:
+            likes[author] = int(likesCnt)
+            posts[author] = 1
 
-d = {}
-for ele in allTag:
-    try:
-        d[ele] += 1
-    except:
-        d[ele] = 1
+for author in likes:
+    print( author + ' ' + str(likes[author]/posts[author]) )
+# allTag = []
+# for i in range(1, len(data)):
+    # allTag += jieba.analyse.extract_tags(data[i][1],topK=3)
 
-d = sorted(d.items(), key=operator.itemgetter(1))
+# d = {}
+# for ele in allTag:
+    # try:
+        # d[ele] += 1
+    # except:
+        # d[ele] = 1
 
-for ele in d:
-    print(ele[0])
+# d = sorted(d.items(), key=operator.itemgetter(1))
+
+# for ele in d:
+    # print(ele[0])
